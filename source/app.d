40c48f30be3e6c4111fed7df7e325500816eae1c
Emil Nicolaie Perhinschi <emilper@gmail.com>
2021-12-01 16:54:52 +0200

    basic stopwatch works

--- source/app.d
+++ source/app.d
@@ -12,14 +12,13 @@ import gtk.Box;
 import gtk.Widget;
 import cairo.Context;
 import gtk.DrawingArea;
-import glib.Timeout;
 import gtk.Button;
 
 void main(string[] args) {
 
 	Main.init(args);
 
-	StopwatchWindow w = new StopwatchWindow("SDL test");    
+	StopwatchWindow w = new StopwatchWindow("Stopwatch");    
     auto box = new Box(Orientation.VERTICAL, 10);
 
 	auto displayTime = new DisplayTime();
@@ -27,19 +26,11 @@ void main(string[] args) {
     auto buttonsContainer = new Box(Orientation.HORIZONTAL, 10);
     
     auto startButton = new Button("Start");
-    startButton.addOnClicked(delegate void(Button b) { writeln("Start"); });
+    startButton.addOnClicked(delegate void(Button b) { displayTime.toggleTimer(startButton); });
     buttonsContainer.packStart(startButton, true, true, 0);
 
-    auto pauseButton = new Button("Pause");
-    pauseButton.addOnClicked(delegate void(Button b) { writeln("Pause"); }); 
-    buttonsContainer.packStart(pauseButton, true, true, 0);
-
-    auto stopButton = new Button("Stop");
-    stopButton.addOnClicked(delegate void(Button b) { writeln("Stop"); }); 
-    buttonsContainer.packStart(stopButton, true, true, 0);
-
     auto resetButton = new Button("Reset");
-    resetButton.addOnClicked(delegate void(Button b) { writeln("Reset"); }); 
+    resetButton.addOnClicked(delegate void(Button b) { displayTime.reset(startButton); }); 
     buttonsContainer.packStart(resetButton, true, true, 0);
 
     box.packStart(buttonsContainer, false, true, 0);
@@ -63,16 +54,48 @@ class StopwatchWindow : MainWindow {
 }
 
 class DisplayTime : DrawingArea {
+    import std.datetime: Duration;
+    import std.datetime.stopwatch: StopWatch, AutoStart;
+    import glib.Timeout;
+
+    bool paused;
     Timeout timeout;
 	int fps = 1000 / 30;
-    MonoTime startTime;
+    StopWatch timer;
     
     this() {
         this.addOnDraw(&onDraw);
+        this.timer = StopWatch(AutoStart.no);
+    }
+
+    void toggleTimer(Button timerButton) {
+        if (this.timer.running) {
+            debug writeln("Stop");
+            this.paused = true;
+            this.timer.stop();
+            timerButton.setLabel("Restart");
+        } else {
+            this.paused = false;
+            debug writeln("Restarting"); 
+            this.timer.start();
+            timerButton.setLabel("Pause");
+        }
+    }
+
+    void reset(Button startButton) {
+        debug writeln("Reset"); 
+        if (this.timer.running()) {
+            this.timer.stop();
+        }
+        this.timer.reset();
+        startButton.setLabel("Start");
     }
 
     bool onDraw(Scoped!Context context, Widget w)
     {
+        import std.conv: to;
+        // import std.datetime.systime: Clock, SysTime, Duration;
+
         if(timeout is null)
         {
             timeout = new Timeout(fps, &onFrameElapsed, false);
@@ -81,15 +104,17 @@ class DisplayTime : DrawingArea {
         context.selectFontFace("Helvetica", CairoFontSlant.NORMAL, CairoFontWeight.NORMAL);
         context.setFontSize(65);
         context.setSourceRgb(0.0, 0.0, 0.0);
-        context.moveTo(280, 170);
-        
-        auto 
-        
-        context.showText(number.to!string());
-        number++;
 
+        GtkAllocation allocation;
+        int baseline;
+        this.getAllocatedSize(allocation, baseline);
+        context.moveTo(30, allocation.height/2);
+        int seconds;
+        short miliseconds;
+        auto elapsed_so_far = this.timer.peek();
+        elapsed_so_far.split!("seconds", "msecs")(seconds, miliseconds);
+        context.showText(seconds.to!string ~ ":" ~ miliseconds.to!string);
         return(true);
-        
     }
 
     bool onFrameElapsed()
